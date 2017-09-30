c751a4d9a8914c0b91f1743509ff3cc92dfc9461
Kenan Warren <kenan.warren@escribir.io>
2017-09-30 12:38:10 -0700

    Initial UE Project

---
+++ UETanks/Source/UETanks/UETanksGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UETanksGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UETANKS_API AUETanksGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
